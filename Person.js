d14da3d953b7321f0e26265475f3e7cb0406268f
hard-l0zzz <117300753+hard-l0zzz@users.noreply.github.com>
2022-12-09 15:43:13 +0700

    Add files via upload

--- Person.js
+++ Person.js
@@ -19,7 +19,7 @@ class Person extends GameObject{
         }
         else
         {
-            if(!state.map.isCutScenePlaying && this.isPlayerControlled && state.arrow){
+            if( !state.map.isCutscenePlaying && this.isPlayerControlled && state.arrow){
                 this.startBehavior(state, {
                     type:"walk",
                     direction: state.arrow
