e9f1deec8c825eaae0b511b3d9e6cf7575ad2142
hard-l0zzz <117300753+hard-l0zzz@users.noreply.github.com>
2023-01-31 19:45:05 +0700

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,47 @@
+window.PizzaTypes = {
+  normal: "normal",
+  spicy: "spicy",
+  veggie: "veggie",
+  fungi: "fungi",
+  chill: "chill",
+}
+
+window.Pizzas = {
+  "s001": {
+    name: "Пицца пеперони",
+    type: PizzaTypes.spicy,
+    description:"Классика",
+    src: "/images/characters/pizzas/s001.png",
+    icon: "/images/icons/spicy.png",
+    actions:[/*"test",*/"damage1","clumsyStatus","saucyStatus"]
+  },
+  "s002": {
+    name: "Пицца с беконом",
+    description:"Эпичный бекон, солёное буйство!",
+    type: PizzaTypes.spicy,
+    src: "/images/characters/pizzas/s002.png",
+    icon: "/images/icons/spicy.png",
+    actions:["test","damage1","clumsyStatus","saucyStatus",]
+  },
+  "v001": {
+    name: "Пицца с огурчиками",
+    type: PizzaTypes.veggie,
+    src: "/images/characters/pizzas/v001.png",
+    icon: "/images/icons/veggie.png",
+    actions:["damage1","clumsyStatus","item_recoverHp1"]
+  },
+  "f001": {
+    name: "Портобелло Экспресс",
+    type: PizzaTypes.fungi,
+    src: "/images/characters/pizzas/f001.png",
+    icon: "/images/icons/fungi.png",
+    actions:["damage1","clumsyStatus","item_recoverHp1"]
+  },
+  "stepalox": {
+    name: "Степа Лошара",
+    type: PizzaTypes.fungi,
+    src: "/images/characters/pizzas/stepalox2.png",
+    icon: "/images/icons/veggie.png",
+    actions:["damage1","damage1","damage1","damage1"]
+  }
+}
