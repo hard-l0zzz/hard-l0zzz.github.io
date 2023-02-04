635212ecfa08baa8bc5fc80239ab1be7036fec59
hard-l0zzz <117300753+hard-l0zzz@users.noreply.github.com>
2023-02-04 12:28:22 +0700

    Add files via upload

--- State/PlayerState.js
+++ State/PlayerState.js
@@ -10,17 +10,17 @@ class PlayerState {
                 level: 1,
                 status: null
             },
-            "p2":{
-                pizzaId:"s002",
-                hp: 50,
-                maxHp: 50,
-                xp: 0,
-                maxXp: 100,
-                level: 1,
-                status: null
-            }
+            // "p2":{
+            //     pizzaId:"s002",
+            //     hp: 50,
+            //     maxHp: 50,
+            //     xp: 0,
+            //     maxXp: 100,
+            //     level: 1,
+            //     status: null
+            // }
         }
-        this.lineup = ["p1", "p2"];
+        this.lineup = ["p1"];
         this.items = [
             { actionId: "item_recoverHp", instanceId: "item1" },
             { actionId: "item_recoverHp", instanceId: "item2" },
